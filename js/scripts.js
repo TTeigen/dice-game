1c4b62ddfffe0f5b2008e3bef6191d3dd67e6029
Marqis Ramos and Trevor Teigen <trevor@>
2019-06-19 16:23:56 -0700

    still suffering, however basic logic works

--- js/scripts.js
+++ js/scripts.js
@@ -23,10 +23,6 @@ Die.prototype.Roll = function (){
   return roll;
 }
 
-Die.prototype.Save = function(die) {
-  this.save = true;
-}
-
 Pool.prototype.assignId = function() {
   this.currentId +=1;
   return this.currentId;
@@ -37,32 +33,112 @@ Pool.prototype.addToPool = function(die){
   this.dice.push(die);
   }
 
+var savedDice = [];
+var roundTotal = 0;
+var total = 0;
+var tally;
+
+function countDice() {
+  let playerRoll = {one: 0, two: 0, three: 0, four: 0, five: 0, six: 0}
+  for (let i=0; i < savedDice.length; i++){
+    if(savedDice[i] === 1){
+      playerRoll.one++
+    } if (savedDice[i] === 2) {
+      playerRoll.two++
+    }else if (savedDice[i] === 3) {
+      playerRoll.three++
+    }else if (savedDice[i] === 4) {
+      playerRoll.four++
+    }else if (savedDice[i] === 5) {
+      playerRoll.five++
+    }else if (savedDice[i] === 6) {
+      playerRoll.six++
+    }
+
+  }
+  return playerRoll
+}
 
 
+
+
+
+
+}
+
+
+    //Initial casting of 6 dice
   $("#cast").click(function(){
+    savedDice =[];
+    total = 0;
     var pool = new Pool();
-    $("#res").empty();
+    $("#res").show().empty();
     for(i = 0; i <6; i++) {
       var roll = Math.floor(Math.random() * 6) + 1;
       var die = new Die(roll);
-      pool.addToPool(die);
-      $("#res").append("<li id='"+i+"'>"+die.face+"</li>")
+      pool.addToPool(die); //adds dice to pool
+      $("#res").prepend("<li id='"+i+"'>"+die.face+"</li>")
     }
-    $("#res").append("<button type='submit' id='reroll'>reroll</button>")
-
     console.log(pool);
-
+      //Click the dice to 'save' it
     $("li").click(function(){
       if (pool.dice[this.id].save == false) {
         pool.dice[this.id].save = true;
+        $("li#"+this.id).addClass('save')
       } else {
         pool.dice[this.id].save = false
+        $("li#"+this.id).removeClass('save')
       }
-      console.log(pool.dice[this.id]);
+      // console.log(pool.dice[this.id]);
     })
+      //rerolls 'unsaved' dice and moves 'saved' dice to right
+    $("#reroll").click(function(){
+      savedDice =[];
+      for (i=0; i<6; i++) {
+        if (pool.dice[i].save == false) {
+          pool.dice[i].face = die.Roll();
+          console.log(pool.dice[i]);
+          $("li#"+i).text(pool.dice[i].face)
+        }
+      }
+      console.log(savedDice);
+    })
+      //scores saved dice and ends turn
+
+    $("#score").click(function(){
+      for (i=0; i<6; i++) {
+        if (pool.dice[i].save == true){
+          savedDice.push(pool.dice[i].face)
+          pool.dice[i].save = "scored";
+        }
+      }
+      tally = countDice(savedDice);
+      roundTotal = addScore(tally);
+      console.log(tally);
+    });
   })
 
 
+
+
+  // function score() {
+  //   var singleOne = 100;
+  //   var singleFive = 50;
+  //   var triple = 100*face
+  //   for (pool.dice) {
+  //     if save < 3 {
+  //       test 1 vs 5 add 100 or 50
+  //     } else if save ==3 {
+  //       this.id * 100
+  //     }else if save ==4 {
+  //       this.id*200
+  //     } else if (save =5) {
+  //       this.id*400
+  //     } else {
+  //       add 6 score
+  //     }
+  //     }
+  //   }
 
 
 
