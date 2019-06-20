10ad1c1e87e0910088c224147e8b9f8d9012f317
Marqis Ramos and Trevor Teigen <marqis@>
2019-06-20 14:43:47 -0700

    play the game

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,41 @@ var points = 0;
 var total = 0;
 var pool = new Pool;
 var savedPool = new Pool;
+var players = [];
+var player1;
+var player2;
 
+//Pool objects & prototypes *******
 function Pool (one, two, three, four, five, six) {this.one = 0, this.two = 0, this.three = 0, this.four = 0, this.five = 0, this.six = 0};
 
+Pool.prototype.clearPool = function(){
+  this.one = 0,
+  this.two = 0,
+  this.three = 0,
+  this.four = 0,
+  this.five = 0,
+  this.six = 0
+}
+//**********************************
+
+//Player Objects & prototypes*************************
+function Player (name, total) {
+  this.name = name,
+  this.total = total,
+  this.active = false
+}
+
+
+Player.prototype.makeActive = function() {
+    if (this.active === false) {
+      this.active = true;
+    } else {
+      this.active = false;
+      this.total += points;
+    }
+}
+
+//***********************
 
 
 $(function(){
@@ -24,6 +56,7 @@ function rollDice() {
 }
 //counts dice faces & stores in dice object for initial pool
 function poolDice() {
+  pool.clearPool();
   for (let i=0; i < dice.length; i++){
     if(dice[i] === 1){
       pool.one++
@@ -40,7 +73,9 @@ function poolDice() {
     }
   }
 }
+//creates a pool of saved dice values
 function saveDice() {
+  savedPool.clearPool();
   for (let i=0; i < saved.length; i++){
     if(saved[i] === 1){
       savedPool.one++
@@ -58,11 +93,12 @@ function saveDice() {
   }
 }
 //math for scoring points
-function math(pool) {
+function math(savedPool) {
   counter = 0;
- for (var face in pool) {
+  console.log(savedPool);
+ for (var property in savedPool) {
    counter++;
-   if (pool[face] === 1) {
+   if (savedPool[property] === 1) {
      if (counter == 1) {
        points += 100;
      }
@@ -72,7 +108,7 @@ function math(pool) {
        alert ("invalid single");
      }
    }
-   if (pool[face] === 2) {
+   if (savedPool[property] === 2) {
      if (counter == 1) {
        points += 200;
      } else if (counter == 5) {
@@ -81,75 +117,82 @@ function math(pool) {
        alert ("invalid double");
      }
    }
-   if (pool[face] === 3) {
+   if (savedPool[property] === 3) {
      if (counter == 1) {
        points+= 1000;
      } else {
        points += counter*100;
      }
    }
-   if (pool[face] === 4) {
-     if (counter == 1) {
+   if (savedPool[property] === 4) {
+     if (counter === 1) {
        points+= 2000;
      } else {
        points += counter*200;
      }
    }
-   if (pool[face] === 5) {
-     if (counter == 1) {
+   if (savedPool[property] === 5) {
+     if (counter === 1) {
        points+= 3000;
      } else {
        points += counter*400;
      }
    }
-   if (pool[face] == 6) {
-    alert ("Cannot have 6 of a kind, return 1 die.")
-   }
+
    console.log("counter "+counter);
+   console.log("points "+points);
  }
- console.log("points "+points);
- return points;
 }
 
-// function scoreDice() {
-//   math(savedPool);
-//   console.log("saved " + saved);
-//   console.log(savedPool);
-//
-// }
+$(".player").submit(function(event){
+  event.preventDefault();
+  player1 = new Player ($("#player1").val(), total)
+  player2 = new Player ($("#player2").val(), total)
+  player1.active = true;
+  players.push(player1, player2);
+  console.log(players);
+})
 
+$("#cast").click(function(){
+  $("#res").empty();
+  rollDice();
+  poolDice();
+  for (i=0; i <dice.length ;i++){
+    $("#res").append("<input type='checkbox' value = '"+dice[i]+"'>"+dice[i]);
+  }
+  console.log("castclicked");
+  console.log(pool);
+})
+ //when dice are scored splice from pool then reroll values of current pool
+ $("#save").click(function(){
+   saved = []
+   $("#res input:checked").each(function(){
+     saved.push(parseInt($(this).val()));
+     dice.splice(0,1);
+     console.log("saveclicked");
+   });
+   saveDice();
+   math(savedPool);
+ })
 
-
-
-
-
- $("#cast").click(function(){
+ $("#reroll").click(function(){
    $("#res").empty();
-   rollDice();
-   poolDice();
-   for (i=0; i <dice.length ;i++){
+   for (i=0; i < dice.length;i++) {
+     var newRoll = Math.floor(Math.random() * 6) + 1;
+     dice.splice(i, 1, newRoll)
      $("#res").append("<input type='checkbox' value = '"+dice[i]+"'>"+dice[i]);
    }
-   console.log("castclicked");
-   console.log(pool);
+   poolDice();
+   if (pool.one < 1 && pool.five < 1 && pool.two < 3 && pool.three < 3 && pool.four < 3 && pool.six < 3 ) {
+     alert("YOU BUST!");
+     points = 0;
+   }
  })
-  //when dice are scored splice from pool then reroll values of current pool
-  $("#save").click(function(){
-    $("#res input:checked").each(function(){
-      saved.push(parseInt($(this).val()));
-      dice.splice(0,1);
-      console.log("saveclicked");
-    });
-    saveDice();
-    math(savedPool);
-  })
-
-  $("#reroll").click(function(){
-    $("#res").empty();
-    for (i=0; i < dice.length;i++) {
-      var newRoll = Math.floor(Math.random() * 6) + 1;
-      dice.splice(i, 1, newRoll)
-      $("#res").append("<input type='checkbox' value = '"+dice[i]+"'>"+dice[i]);
-    }
-  })
+ $("#turn").click(function(){
+   player1.makeActive();
+   console.log(player1);
+   player2.makeActive();
+   console.log(player2);
+   points = 0;
+ })
 });
